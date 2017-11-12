8d50ea89e11197bf0de8d5d29f5dc15c36550202
Eitenne <finnurs@protonmail.ch>
2017-11-12 01:53:40 +0100

    Update update.py

--- update.py
+++ update.py
@@ -6,7 +6,7 @@ import os
 def update():
 	print "[*]Updating roxysploit framework, Please Wait ..."
 	try:
-		subprocess.Popen("cd /tmp;wget http://xl65ae7epcwr2z58.rf.gd/rsf.zip ;unzip rsf.zip; rm rsf.zip", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
+		subprocess.Popen("cd /opt;git clone https://github.com/Eitenne/roxysploit.git", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).wait()
 	except Exception, e:
 		print "[!] Update Failed."
 		pass
