acd308a465e685b6a52ab06719f9d737cbe9ad7f
Eitenne <finnurs@protonmail.ch>
2018-02-21 15:57:29 +0100

    Update modules.py

--- core/modules.py
+++ core/modules.py
@@ -10,8 +10,47 @@ def all():
     print "====================\n"
     print " Name"
     print " ----"
-    directory_list = glob.glob('plugins/*.plugin')
+    directory_list = glob.glob('plugins/Exploits/*.plugin')
     for line in directory_list:
-        print " " + line.split('plugins/')[1].split('.plugin')[0]
-        
+        print " " + line.split('plugins/Exploits/')[1].split('.plugin')[0]
+    directory_list = glob.glob('plugins/Payloads/*.plugin')
+    for line in directory_list:
+        print " " + line.split('plugins/Payloads/')[1].split('.plugin')[0]
+    directory_list = glob.glob('plugins/Utilities/*.plugin')
+    for line in directory_list:
+        print " " + line.split('plugins/Utilities/')[1].split('.plugin')[0]
+
     print ""
+
+def exploits():
+    print ""
+    print "Plugin Category: Exploits"
+    print "====================\n"
+    print " Name"
+    print " ----"
+    directory_list = glob.glob('plugins/Exploits/*.plugin')
+    for line in directory_list:
+        print " " + line.split('plugins/Exploits/')[1].split('.plugin')[0]
+    print " "
+
+def payloads():
+    print ""
+    print "Plugin Category: Payloads"
+    print "====================\n"
+    print " Name"
+    print " ----"
+    directory_list = glob.glob('plugins/Payloads/*.plugin')
+    for line in directory_list:
+        print " " + line.split('plugins/Payloads/')[1].split('.plugin')[0]
+    print " "
+
+def utilities():
+    print ""
+    print "Plugin Category: Utilities"
+    print "====================\n"
+    print " Name"
+    print " ----"
+    directory_list = glob.glob('plugins/Utilities/*.plugin')
+    for line in directory_list:
+        print " " + line.split('plugins/Utilities/')[1].split('.plugin')[0]
+    print " "
