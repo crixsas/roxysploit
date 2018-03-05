c2ce2d723f85bd3ddc57a9dc2076b7a56d69753b
Eitenne <finnurs@protonmail.ch>
2018-03-05 04:35:53 +0100

    Update pluginfinder.py

--- core/pluginfinder.py
+++ core/pluginfinder.py
@@ -4,8 +4,8 @@ import glob
 import sys
 from subprocess import check_output
 
-os.system('find plugins/*.plugin -maxdepth 1 -type f | xargs -iZ basename Z | cut -d"." -f1 > storage/plugins.loaded')
-x = open("storage/plugins.loaded", "r")
+
+x = pass
 #ripas = os.path.splitext(content)[-2]
 #datasw = ripas.split('/')[1]
 
